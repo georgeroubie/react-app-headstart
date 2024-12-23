2012bce40f8da13d55184a46ace0ba02d33c79c1
George Roubie <george.roubie@gmail.com>
2024-12-23 21:30:19 +0200

    Add pwa and sitemap configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import './App.css';
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -9,9 +8,6 @@ const App = () => {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,88 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+          },
+          entryFileNames: `[name].${env.npm_package_version}.js`,
+          chunkFileNames: `[name].${env.npm_package_version}.js`,
+          assetFileNames: `[name].${env.npm_package_version}.[ext]`,
+        },
+      },
+    },
+    plugins: [
+      react(),
+      Sitemap({
+        hostname: env.VITE_PUBLIC_URL,
+        dynamicRoutes: JSON.parse(env.VITE_SITEMAP_URLS || ''),
+        robots: [
+          {
+            userAgent: '*',
+            allow: JSON.parse(env.VITE_ALLOW_ROBOTS_URLS || ''),
+            disallow: JSON.parse(env.VITE_DISALLOW_ROBOTS_URLS || ''),
+          },
+        ],
+      }),
+      VitePWA({
+        useCredentials: true,
+        registerType: 'autoUpdate',
+        includeAssets: [
+          'favicon.ico',
+          'favicon.svg',
+          'pwa-64x64.png',
+          'apple-touch-icon-180x180.png',
+          'pwa-192x192.png',
+          'maskable-icon-512x512.png',
+          'pwa-512x512.png',
+        ],
+        manifest: {
+          id: env.npm_package_version,
+          lang: env.VITE_DEFAULT_LANGUAGE,
+          start_url: '/',
+          display: 'standalone',
+          name: env.VITE_APP_NAME,
+          short_name: env.VITE_APP_SHORT_NAME,
+          description: env.VITE_APP_DESCRIPTION,
+          theme_color: '#' + env.VITE_THEME_COLOR,
+          background_color: '#' + env.VITE_BACKGROUND_COLOR,
+          icons: [
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable',
+            },
+          ],
+        },
+      }),
+    ],
+  };
 });
