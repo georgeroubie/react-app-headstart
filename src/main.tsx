c47871c18d94534daa086a54da19fc6fc09d6f93
George Roubie <george.roubie@gmail.com>
2024-12-25 00:40:14 +0200

    Remove scss mixins and scss deprecation warnings

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app/App';
-import './main.scss';
+import './styles/animations.scss';
+import './styles/base.scss';
+import './styles/normalize.scss';
+import './styles/typography.scss';
+import './styles/variables.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
